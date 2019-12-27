c69d59f47ce4d3d7c595d30e0550870117083a93
n1k-crimea <evloshevsky@gmail.com>
2019-12-27 13:36:22 +0300

    work->home

--- app.py
+++ app.py
@@ -7,7 +7,28 @@ app = Flask(__name__)
 
 meta_data = {'title': title, 'subtitle': subtitle, 'description': description}
 
-#def find_end(n:int, val:str) -> tuple:
+def find_end(n:int, val:str):
+    if val == 'tours':
+        if n in [11, 12, 13, 14]:
+            return 'туров'
+        else:
+            if str(n)[-1] in ['1']:
+                return 'тур'
+            elif str(n)[-1] in ['2', '3', '4']:
+                return 'тура'
+            else:
+                return 'туров'
+    elif val == 'nights':
+        if n in [11, 12, 13, 14]:
+            return 'ночей'
+        else:
+            if str(n)[-1] in ['1']:
+                return 'ночь'
+            elif str(n)[-1] in ['2', '3', '4']:
+                return 'ночи'
+            else:
+                return 'ночей'
+
 
 @app.route('/')
 def index():
@@ -31,7 +52,7 @@ def direction(direction):
         if key == direction:
             departure_ru = val
     count_tours = len(tours_direction)
-    if count_tours in [11, 12, 13, 14]:
+    '''if count_tours in [11, 12, 13, 14]:
         ending = 'туров'
     else:
         if str(count_tours)[-1] in ['1']:
@@ -39,13 +60,13 @@ def direction(direction):
         elif str(count_tours)[-1] in ['2', '3', '4']:
             ending = 'тура'
         else:
-            ending = 'туров'
+            ending = 'туров'''
     price_list = [val['price'] for key, val in tours_direction.items()]
     nights_list = [val['nights'] for key, val in tours_direction.items()]
 
-    meta_departure = {'departure': departure_ru, 'count_tours': count_tours, 'ending': ending,
+    meta_departure = {'departure': departure_ru, 'count_tours': count_tours, 'ending': find_end(count_tours, 'tour'),
                       'min_price': min(price_list),
-                      'max_price': max(price_list), 'min_nights': min(nights_list), 'max_nights': max(nights_list)}
+                      'max_price': max(price_list), 'min_nights': {'count': min(nights_list), 'ending': find_end(min(nights_list), 'nights')}, 'max_nights': {'count': max(nights_list), 'ending': find_end(max(nights_list), 'nights')}}
     return render_template('direction.html', meta_data=meta_data, departures=departures,
                            tours_direction=tours_direction, meta_departure=meta_departure)
 
