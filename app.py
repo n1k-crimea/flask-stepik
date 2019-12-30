490c2a7d264544b961b9cdd11006db4900566648
n1k-crimea <evloshevsky@gmail.com>
2019-12-31 00:23:33 +0300

    add context_processor

--- app.py
+++ app.py
@@ -39,6 +39,11 @@ def find_end(n: int, val: str):
                 return 'ночей'
 
 
+@app.context_processor
+def pass_options():
+    return dict(meta_data=meta_data, departures=departures)
+
+
 @app.route('/')
 def index():
     tours_main_page = {}
@@ -47,7 +52,7 @@ def index():
     rn = rn[:6]
     for i in rn:
         tours_main_page[i] = tours[i]
-    return render_template('index.html', meta_data=meta_data, departures=departures, tours=tours_main_page)
+    return render_template('index.html',  tours=tours_main_page)
 
 
 @app.route('/from/<direction>')
@@ -61,17 +66,15 @@ def direction(direction):
         if key == direction:
             departure_ru = val
     count_tours = len(tours_direction)
-    price_list = [val['price'] for key, val in tours_direction.items()]
-    nights_list = [val['nights'] for key, val in tours_direction.items()]
     meta_departure = {'departure': departure_ru, 'count_tours': count_tours,
                       'ending': find_end(count_tours, 'count_tours'),
-                      'min_price': min(price_list),
-                      'max_price': max(price_list),
-                      'min_nights': {'count': min(nights_list),
-                                     'ending': find_end(min(nights_list), 'nights_departures')},
-                      'max_nights': {'count': max(nights_list),
-                                     'ending': find_end(max(nights_list), 'nights_departures')}}
-    return render_template('direction.html', meta_data=meta_data, departures=departures,
+                      'min_price': min([val['price'] for key, val in tours_direction.items()]),
+                      'max_price': max([val['price'] for key, val in tours_direction.items()]),
+                      'min_nights': {'count': min([val['nights'] for key, val in tours_direction.items()]),
+                                     'ending': find_end(min([val['nights'] for key, val in tours_direction.items()]), 'nights_departures')},
+                      'max_nights': {'count': max([val['nights'] for key, val in tours_direction.items()]),
+                                     'ending': find_end(max([val['nights'] for key, val in tours_direction.items()]), 'nights_departures')}}
+    return render_template('direction.html',
                            tours_direction=tours_direction, meta_departure=meta_departure)
 
 
@@ -80,8 +83,8 @@ def tour(id):
     tour = tours[id].copy()
     tour['departure'] = departures[tour['departure']]
     tour['nights'] = str(tour['nights']) + ' ' + find_end(tour['nights'], 'nights_tour')
-    return render_template('tour.html', meta_data=meta_data, departures=departures, tour=tour)
+    return render_template('tour.html', tour=tour)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
